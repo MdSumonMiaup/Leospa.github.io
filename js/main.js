91c3e646d3d1d061b2b18276dd6241eb221b33d4
SamrtDesign <SamrtDesign@users.noreply.github.com>
2020-03-31 11:48:51 +0600

    Leospa

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+
+});
+
+
+
+
+(function($){
+  'use strict';
+    $(window).on('load', function () {
+        if ($(".pre-loader").length > 0)
+        {
+            $(".pre-loader").fadeOut("slow");
+        }
+    });
+})(jQuery)
