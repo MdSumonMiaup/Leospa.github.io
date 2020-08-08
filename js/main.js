b718af9a4eb1630220bf99eb7608ab245ffe6611
SamrtDesign <62637625+SamrtDesign@users.noreply.github.com>
2020-08-08 15:14:41 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,4 +14,12 @@ var loader = function() {
 	loader();
 
 
+$('#nav').onePageNav({
+	currentClass: 'current',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing'
+});
 
